d79c18769081d55a902dde3914e5596cd55b6069
Mylena Assis <lena.assis.dev@gmail.com>
2024-06-25 17:58:40 -0300

    Added: inserir cliente com id aleatorio

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        void opcoesMenu()
+        {
+            Console.WriteLine("*****MENU*****");
+            Console.WriteLine("1. Inserir cliente na fila");
+            Console.WriteLine("2. Atender próximo cliente");
+            Console.WriteLine("3. Exibir próximo cliente");
+            Console.WriteLine("4. Verificar tamanho da fila");
+            Console.WriteLine("5. Sair");
+            Console.WriteLine("Selecione uma opção: ");
+            string opcaoSelecionada = Console.ReadLine()!;
+
+            int opcaoSelecionadaNumerica = int.Parse(opcaoSelecionada);
+
+            switch (opcaoSelecionadaNumerica)
+            {
+                case 1:
+                    InserirCliente();
+                    break;
+                case 2:
+                    //AtenderProximoCliente;
+                    break;
+                case 3:
+                    //ExibirProximoCliente;
+                    break;
+                case 4:
+                    //VerificarTamanhoFila();
+                    break;
+                case 5:
+                    Console.WriteLine("Tchau! =)");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+        }
+
+        void InserirCliente()
+        {
+            Random random = new Random();
+            int idCliente = random.Next();
+            Console.WriteLine(idCliente);
+        }
+
+        opcoesMenu();
+
+    }
+}
