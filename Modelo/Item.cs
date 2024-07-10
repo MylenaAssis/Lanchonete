225fc3f9febaebdb27540940b6e24ab7fe9eb1ba
Mylena Assis <lena.assis.dev@gmail.com>
2024-07-10 06:17:47 -0300

    Added: metodos da classe Item

--- Modelo/Item.cs
+++ Modelo/Item.cs
@@ -9,6 +9,7 @@ namespace FilaPedidos.Modelo
 {
     internal class Item
     {
+        public int Id { get; private set; }
         public string Nome { get; private set; }
         public double Valor { get; private set; }
         public bool Disponivel { get; private set; }
@@ -23,5 +24,8 @@ namespace FilaPedidos.Modelo
 
         //propriedades que tem apenas getter podem ser trocadas por funcoes lambda, como a seguir:
         public string Resumo => $"{Nome} ----- R${Valor}.";
+
+        public void CadastrarNovoItem();
+
     }
 }
